4bb016b02561f675c8c117a7756d866c504bbd05
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-07-01 16:34:04 +0200

    Initial Fix for entrypoint.sh for crc-e2e container

--- images/build-e2e/entrypoint.sh
+++ images/build-e2e/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Vars
-MONICTL_PATH=/usr/local/bin/monictl
+BYNARY=e2e.test
+if [[ ${PLATOFORM} == 'windows' ]]; then
+    BYNARY=e2e.test.exe
+fi
+BINARY_PATH="/opt/crc/bin/${PLATOFORM}-amd64/${CRC_E2E_BYNARY}"
+FEATURES_PATH=/opt/crc/features
 RESULTS_PATH="${RESULTS_PATH:-/output}"
-MONICTL_REPETITIONS="${MONICTL_REPETITIONS:-5}"
-MONICTL_INTERVAL="${MONICTL_INTERVAL:-1}"
 
 if [ "${DEBUG:-}" = "true" ]; then
     set -xuo 
@@ -25,29 +28,39 @@ validate=true
     && validate=false
 [[ $validate == false ]] && exit 1
 
-# Set SCP / SSH command
+# Set SCP / SSH command with pass or key
 NO_STRICT='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 [[ ! -z "${TARGET_HOST_KEY_PATH}" ]] \
-    && SCP="scp ${NO_STRICT} -i ${TARGET_HOST_KEY_PATH}" \
-        || SCP="sshpass -p ${TARGET_HOST_PASSWORD} scp ${NO_STRICT}" \
+    && SCP="scp -r ${NO_STRICT} -i ${TARGET_HOST_KEY_PATH}" \
+        || SCP="sshpass -p ${TARGET_HOST_PASSWORD} scp -r ${NO_STRICT}" \
     && SSH="ssh ${NO_STRICT} -i ${TARGET_HOST_KEY_PATH}" \
         || SSH="sshpass -p ${TARGET_HOST_PASSWORD} ssh ${NO_STRICT}"
 
 # Create execution folder 
-EXECUTION_FOLDER="/home/${TARGET_HOST_USERNAME}/monitools/${RANDOM}"
-DATA_FOLDER="${EXECUTION_FOLDER}/data"
-$SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "mkdir -p ${EXECUTION_FOLDER} && mkdir -p ${DATA_FOLDER}" 
+EXECUTION_FOLDER="/Users/${TARGET_HOST_USERNAME}/crc-e2e/${RANDOM}"
+if [[ ${PLATOFORM} == 'linux' ]]; then
+    EXECUTION_FOLDER="/home/${TARGET_HOST_USERNAME}/crc-e2e/${RANDOM}"
+fi
+DATA_FOLDER="${EXECUTION_FOLDER}/out"
+if [[ ${PLATOFORM} == 'windows' ]]; then
+    # Todo change for powershell cmdlet
+    $SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "mkdir -p ${EXECUTION_FOLDER} 
+else
+    $SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "mkdir -p ${EXECUTION_FOLDER} 
+fi
 
-# Copy monictl to target host
-$SCP "${MONICTL_PATH}" "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${EXECUTION_FOLDER}"
+# Copy crc-e2e binary and features spec to target host
+$SCP "${BINARY_PATH}" "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${EXECUTION_FOLDER}"
+$SCP "${FEATURES_PATH}" "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${EXECUTION_FOLDER}"
 
 # Run (one shot) monictl
-MONITOOL_EXEC="${EXECUTION_FOLDER}/monictl -d ${DATA_FOLDER} -n ${MONICTL_REPETITIONS} -s ${MONICTL_INTERVAL}"
-$SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "${MONITOOL_EXEC}"
+# TODO add run parameters
+BINARY_EXEC="${EXECUTION_FOLDER}/${BYNARY} 
+$SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "${BINARY_EXEC}"
 
 # Get results
 mkdir -p "${RESULTS_PATH}"
-$SCP -r "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${DATA_FOLDER}" "${RESULTS_PATH}"
+$SCP "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${DATA_FOLDER}" "${RESULTS_PATH}"
 
 # Remove remote execution fodler
 $SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" rm -rf "${EXECUTION_FOLDER}"
